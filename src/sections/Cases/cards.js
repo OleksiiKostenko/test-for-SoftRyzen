6f5453d549e666bd74cad501e35ed241fc1ca497
Oleksii Kostenko <110487805+OleksiiKostenko@users.noreply.github.com>
2023-12-05 00:26:57 +0200

    Update cards.js

--- src/sections/Cases/cards.js
+++ src/sections/Cases/cards.js
@@ -1,14 +1,14 @@
 export const cards = [
   {
     id: "1",
-    src: "./public/image/wind_turbine_in_field.png",
+    src: "../public/image/wind_turbine_in_field.png",
     title: "Lviv Region, Radekhiv town Private Enterprise “ZAKHIDNYI BUH”",
     description: "Wind Power for auto field irrigation",
     data: "July 2023",
   },
   {
     id: "2",
-    src: "./public/image/sun_panel_field.png",
+    src: "/public/image/sun_panel_field.png",
     title: "Zhytomyr city Private Enterprise “Bosch”",
     description:
       "Solar Panels for industrial useSolar Panels for industrial use",
@@ -16,21 +16,21 @@ export const cards = [
   },
   {
     id: "3",
-    src: "./public/image/h2_generetor.png",
+    src: "public/image/h2_generetor.png",
     title: "Rivne city Private Enterprise “Biotech”",
     description: "Thermal modules",
     data: "October 2023",
   },
   {
     id: "4",
-    src: "./public/image/wind_turbine_in_desert.png",
+    src: "./../public/image/wind_turbine_in_desert.png",
     title: "Kherson city Private Enterprise “HealthyFarm”",
     description: "Wind power",
     data: "September 2021",
   },
   {
     id: "5",
-    src: "./public/image/tanks.png",
+    src: "/../public/image/tanks.png",
     title: "Zaporizhia city Private Enterprise “Biotech”",
     description: "Mini nuclear stations",
     data: "May 2021",
